00a8dd3ec42e56fed85ecc3159c247735f12df01
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2021-07-21 18:57:40 -0400

    Upload file and added java binaries

--- COMPILER_SERVICE/src/app.js
+++ COMPILER_SERVICE/src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+require('dotenv').config({ path: '../.env'});
 const Execute = require('./core/execute');
 const JavaCommand = require('./core/java_command');
 const JavaParameter =require('./core/java_parameter');
 
 const app = express();
 
-app.get('/compiler', async (req, res) => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const path = '../upload/';
+        fs.mkdirSync(path, { recursive: true});
+        cb(null, path);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    } 
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/compiler', upload.single('file'), async (req, res) => {
     const javaCommand = new JavaCommand();
-    const javaParameter = new JavaParameter('d:/HelloWorld.java', '"C:/Program Files/Java/jdk1.8.0_251/bin/"');
+    console.info(process.env.EXECUTE_JAVA8);
+    const javaParameter = new JavaParameter(req.file.path, process.env.EXECUTE_JAVA8);
     const command = javaCommand.build(javaParameter);
     console.info(command);
     const execute = new Execute();
