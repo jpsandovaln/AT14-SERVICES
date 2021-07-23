4a311d2067d6bc9d3f390d2829b6a15e756684aa
Esther Huarayo Ll <hll.esther@gmail.com>
2021-07-23 11:59:00 -0400

    Integrating converter to endpoint

--- CONVERTER_SERVICE/src/controller/file.controller.js
+++ CONVERTER_SERVICE/src/controller/file.controller.js
@@ -2,6 +2,9 @@ const uploadFile = require("../middleware/upload");
 const fs = require("fs");
 const baseUrl = "http://localhost:8080/files/";
 
+const BuildCmdObtainFrames= require('../model/converter/video/buildCmdObtainFrames');
+const Compiler= require('../model/converter/compiler');
+
 const upload = async (req, res) => {
     try {
         await uploadFile(req, res);
@@ -9,11 +12,20 @@ const upload = async (req, res) => {
         if (req.file == undefined) {
             return res.status(400).send({ message: "Please upload a file!" });
         }
-        res.status(200).send({
+
+        const frames= new BuildCmdObtainFrames();
+        const command= frames.returnCommand('C:/Users/ryzyn/Desktop/ffmepg/ffmpeg.exe','C:/Users/ryzyn/Desktop/ffmepg/canto.mp4','C:/Users/ryzyn/Desktop/ffmepg/','1','flv');
+        const isValid= await Compiler.execute(command);
+
+        if(isValid){
+            
+        }
+
+        /*res.status(200).send({
             name: req.file.originalname,
             url: baseUrl + req.file.originalname,
             params: req.body,
-        });
+        });*/
     } catch (err) {
         console.log(err);
 
