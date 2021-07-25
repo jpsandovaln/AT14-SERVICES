f94205fbceec2cde7dec12242668321fdf419bd7
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-07-25 17:12:13 -0400

    Add comments to the files about what the classes do.

--- CONVERTER_SERVICE/src/model/converter/files/deletePdfCmd.js
+++ CONVERTER_SERVICE/src/model/converter/files/deletePdfCmd.js
@@ -1,15 +1,10 @@
 const path = require("path");
 /**
  * @Class
- * Build a string that is the command to add the option of change quality to the image.
+ * Build a string that is the command to delete an specific file.
  */
 class DeletePdfCmd {
-    /**
-     * @param {string} executablePathConverterPdf The path where executable codec is.
-     * @param {string} pdfPathConvert The pdf path to convert to image.
-     * @param {string} outputPathConverted The image path after converted pdf.
-     * @param {string} outputFormat Output image format.
-     */
+
     returnCommand(pdfPathDelete) {
         const DELETE_COMMAND = "del";
         const SPACE = " ";
