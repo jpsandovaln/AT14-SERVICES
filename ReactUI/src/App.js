e7a2906ec2b4ad6b34998ad4dd479195d264e55b
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-07-25 23:13:10 -0400

    Adding the UI incorporation

--- ReactUI/src/App.js
+++ ReactUI/src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
 import React from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
-import Search from "./components/Search";
+//import Search from "./components/Search";
 import Error404 from "./components/Error404";
 
 function App() {
     return (
+        /*<div className="container">
+            <h4>React Multiple Images Upload with Preview</h4>
+
+            <div className="content">
+                <UploadImages />
+            </div>
+        </div>
         <Router>
             <div className="container mt-5">
                 <div className="btn-group">
@@ -20,6 +29,20 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/Search" component={Search} />
+                    <Route path="" component={Error404} />
+                </Switch>
+            </div>
+        </Router>
+        */
+        <Router>
+            <div className="container mt-5">
+                <div className="btn-group">
+                    <Link to="/" className="btn btn-dark">
+                        Home
+                    </Link>
+                </div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
                     <Route path="" component={Error404} />
                 </Switch>
             </div>
--- ReactUI/src/components/Home.jsx
+++ ReactUI/src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import UploadImages from "./images-upload.component";
 
 const Inicio = () => {
     return (
-        <div>
-            <h1></h1>
+        <div className="container">
+            <h4>React Multiple Images Upload with Preview</h4>
+
+            <div className="content">
+                <UploadImages />
+            </div>
         </div>
     );
 };
