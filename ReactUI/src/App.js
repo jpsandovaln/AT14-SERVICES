54e7c5245e7d1599daab4c3baff6ffa84161eb0b
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-07-26 01:22:29 -0400

    Merging ReactUI with UIHooks

--- ReactUI/src/App.js
+++ ReactUI/src/App.js
@@ -3,24 +3,28 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Search from "./components/Search"
 import Home from "./components/Home";
 import Error404 from "./components/Error404";
 
 function App() {
     return (
-
         <Router>
-            <div className="container mt-5">
+            <div class="container">
                 <div className="btn-group">
                     <Link to="/" className="btn btn-dark">
                         Home
                     </Link>
+                    <Link to="/Search" className="btn btn-dark">
+                        Search
+                    </Link>
                 </div>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="" component={Error404} />
-                </Switch>
             </div>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Search" component={Search} />
+                <Route path="" component={Error404} />
+            </Switch>
         </Router>
     );
 }
