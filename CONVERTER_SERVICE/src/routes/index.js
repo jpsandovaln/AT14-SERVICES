fff4dd64415590e70441ff02ba1d9d57a6129b25
Sergio <serqio.kay@gmail.com>
2021-08-02 11:08:48 -0400

    resolving conflicts between branches

--- CONVERTER_SERVICE/src/routes/index.js
+++ CONVERTER_SERVICE/src/routes/index.js
@@ -8,6 +8,38 @@ class Routes {
         router.post("/upload", controller.upload);
         router.get("/files", controller.getListFiles);
         router.get("/files/:name", controller.download);
+        router.post(
+            "/ChangeImageFormat",
+            controllerImagenes.endPointToChangeImageFormat
+        );
+        router.post(
+            "/ChangeImageDirection",
+            controllerImagenes.endPointToChangeImageDirection
+        );
+        router.post(
+            "/ChangeImageDoubling",
+            controllerImagenes.endPointToChangeImageDoubling
+        );
+        router.post(
+            "/ChangeImageMonochrome",
+            controllerImagenes.endPointToChangeImageMonochrome
+        );
+        router.post(
+            "/ChangeImagePaint",
+            controllerImagenes.endPointToChangeImagePaint
+        );
+        router.post(
+            "/ChangeImageQuality",
+            controllerImagenes.endPointToChangeImageQuality
+        );
+        router.post(
+            "/ChangeImageResize",
+            controllerImagenes.endPointToChangeImageResize
+        );
+        router.post(
+            "/ChangeImageToGrayscale",
+            controllerImagenes.endPointToChangeImageToGrayscale
+        );
         app.use(router);
     }
 }
