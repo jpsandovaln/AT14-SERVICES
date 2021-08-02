a9b708f265d529babec80b1e5120aa0cc1d7bf7d
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-01 23:56:10 -0400

    Adding conexion to database in container docker

--- CONVERTER_SERVICE/server.js
+++ CONVERTER_SERVICE/server.js
@@ -2,6 +2,11 @@ const cors = require("cors");
 const express = require("express");
 const Routes = require("./src/routes");
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27018/converterDB')
+    .then(db => console.log('Db is connected to', db.connection.host))
+    .catch(err=> console.error(err));
 
 global.__basedir = __dirname;
 
