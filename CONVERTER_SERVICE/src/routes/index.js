84bac0b26ae4e840d6e8c630237831c54936bb32
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-05 00:15:04 -0400

    Adding routes for endpoints created: find data by Id, delete data by Id and update data by Id

--- CONVERTER_SERVICE/src/routes/index.js
+++ CONVERTER_SERVICE/src/routes/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const controllerImagenes = require("../controller/ImagesController");
 const controller = require("../controller/file.controller");
-const {getVideos}= require('../controller/file.controller');
+const {getData, deleteDataById, findDataById, updateDataById}= require('../controller/file.controller');
 class Routes {
     constructor(app) {
         router.post("/converter", controller.upload);
         router.post("/upload", controller.upload);
         router.get("/files", controller.getListFiles);
         router.get("/files/:name", controller.download);
-        router.get('/videos', getVideos);
+        router.get('/data', getData);
+        router.delete('/delete/:id', deleteDataById);
+        router.get('/find/:id', findDataById);
+        router.patch('/update/:id', updateDataById);
         router.post(
             "/ChangeImageFormat",
             controllerImagenes.endPointToChangeImageFormat
