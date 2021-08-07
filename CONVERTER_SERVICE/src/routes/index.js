0c961e29b3c352bdecbc2acae8a1b676f88de0fb
Milena-Rodriguez-Montecinos <67847182+Milena-Rodriguez-Montecinos@users.noreply.github.com>
2021-08-07 04:15:43 -0400

    Refactor index.js with the current video EndPoint.

--- CONVERTER_SERVICE/src/routes/index.js
+++ CONVERTER_SERVICE/src/routes/index.js
@@ -1,50 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const controllerImagenes = require("../controller/ImagesController");
+const changeVideoFormat = require("../controller/VideoController");
 const controller = require("../controller/file.controller");
-const {getData, deleteDataById, findDataById, updateDataById}= require('../controller/file.controller');
+const {
+    getData,
+    deleteDataById,
+    findDataById,
+    updateDataById,
+} = require("../controller/file.controller");
+
 class Routes {
     constructor(app) {
+        
+        router.post("/videoConverter", changeVideoFormat);
         router.post("/converter", controller.upload);
         router.post("/upload", controller.upload);
         router.get("/files", controller.getListFiles);
         router.get("/files/:name", controller.download);
-        router.get('/data', getData);
-        router.delete('/delete/:id', deleteDataById);
-        router.get('/find/:id', findDataById);
-        router.put('/update/:id', updateDataById);
-        router.post(
-            "/ChangeImageFormat",
-            controllerImagenes.endPointToChangeImageFormat
-        );
-        router.post(
-            "/ChangeImageDirection",
-            controllerImagenes.endPointToChangeImageDirection
-        );
-        router.post(
-            "/ChangeImageDoubling",
-            controllerImagenes.endPointToChangeImageDoubling
-        );
-        router.post(
-            "/ChangeImageMonochrome",
-            controllerImagenes.endPointToChangeImageMonochrome
-        );
-        router.post(
-            "/ChangeImagePaint",
-            controllerImagenes.endPointToChangeImagePaint
-        );
-        router.post(
-            "/ChangeImageQuality",
-            controllerImagenes.endPointToChangeImageQuality
-        );
-        router.post(
-            "/ChangeImageResize",
-            controllerImagenes.endPointToChangeImageResize
-        );
-        router.post(
-            "/ChangeImageToGrayscale",
-            controllerImagenes.endPointToChangeImageToGrayscale
-        );
+        router.get("/data", getData);
+        router.delete("/delete/:id", deleteDataById);
+        router.get("/find/:id", findDataById);
+        router.put("/update/:id", updateDataById);
+
         app.use(router);
     }
 }
