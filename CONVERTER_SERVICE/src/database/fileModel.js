e808d52aeae1941896a460c4db5df8b69830bbfe
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-06 23:11:23 -0400

    Modify file name from database to fileModel

---
+++ CONVERTER_SERVICE/src/database/fileModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const FileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  filePath:{
+    type: String,
+    required: true,
+  },
+});
+
+const FileModel = mongoose.model("File", FileSchema );
+
+module.exports = FileModel;
