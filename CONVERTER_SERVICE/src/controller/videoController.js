9fcf5981991439f6ea61ad6c3e6cac03137e7aef
Milena-Rodriguez-Montecinos <67847182+Milena-Rodriguez-Montecinos@users.noreply.github.com>
2021-08-07 04:09:26 -0400

    add the EndPoint to render video.

---
+++ CONVERTER_SERVICE/src/controller/videoController.js
@@ -0,0 +1,62 @@
+const BuildCmdChangeVideoFormat = require("../model/converter/video/buildCmdChangeVideoFormat");
+const BuildCmdObtainFrames = require("../model/converter/video/buildCmdObtainFrames");
+const BuildCmdObtainAudio = require("../model/converter/video/buildCmdObtainAudio");
+const uploadFilesMiddleware = require("../middleware/uploadFiles");
+const Compiler = require("../model/compiler");
+const path = require("path");
+var fs = require("fs");
+
+const changeVideoFormat = async (req, res) => {
+    await uploadFilesMiddleware(req, res);
+    const VideoNameFile = req.file.filename;
+    const videoPath = __dirname + "/../middleware/resource/" + VideoNameFile;
+    const codecPath = __dirname + "/../../thirdParty/ffmpeg.exe";
+    const outputPath = __dirname + "/../middleware/filesProcessor/";
+    const outputFormat = req.body.outputFormat;
+    const outputFrames = req.body.outputFormatFrames;
+
+    compiler = new Compiler();
+
+    const cmdVideoFormat = BuildCmdChangeVideoFormat.returnCommand(
+        codecPath,
+        videoPath,
+        req.body,
+        outputPath,
+        outputFormat
+    );
+    await compiler.execute(cmdVideoFormat);
+    const resultPathVideoFormat =
+        outputPath + path.parse(VideoNameFile).name + outputFormat;
+
+    const dir = outputPath + path.parse(VideoNameFile).name;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    const cmdObtainFrames = BuildCmdObtainFrames.returnCommand(
+        codecPath,
+        videoPath,
+        req.body,
+        dir + "/",
+        outputFrames
+    );
+    await compiler.execute(cmdObtainFrames);
+    const resultPathFrames = dir;
+
+    const cmdObtainAudio = BuildCmdObtainAudio.returnCommand(
+        codecPath,
+        videoPath,
+        outputPath
+    );
+    await compiler.execute(cmdObtainAudio);
+    const resultPathAudio = outputPath + path.parse(VideoNameFile).name + ".mp3";
+
+    res.send([
+        { message: resultPathVideoFormat },
+        { message: resultPathFrames },
+        { message: resultPathAudio },
+    ]);
+
+    zipping.zipDownload(req, res);
+};
+
+module.exports = changeVideoFormat;
