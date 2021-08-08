3ee965244477813d4b2468856f051b50f57298c2
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-08 03:54:32 -0400

    Add express json to recognize incoming request object as json object

--- CONVERTER_SERVICE/server.js
+++ CONVERTER_SERVICE/server.js
@@ -16,6 +16,7 @@ class Index {
         port = 8080
     ) {
         app.use(cors(corsOptions));
+        app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
         new Routes(app);
         this.init(port);
