c68b4b6ef693ccc489fa2298c58d06b41700f24d
Sergio <serqio.kay@gmail.com>
2021-08-08 16:46:37 -0400

    add mobilenet and cocossd test

---
+++ Test/MachineLearning/middleWare/analize.test.js
@@ -0,0 +1,44 @@
+const analize = require("../../../MLendPoint/src/middleware/analize");
+
+const analizing = new analize(
+    "imagePaths",
+    "searchwords",
+    "percentage",
+    "secondsToString",
+    "algorithm",
+    "pathImage"
+);
+
+describe("analize.js", () => {
+    test("Verify method get imagePaths is recibing the right value ", () => {
+        expect(analizing.imagePaths).toBe("imagePaths");
+    });
+
+    test("Verify method get imagePaths is recibing a wrong value ", () => {
+        expect(analizing.imagePaths).not.toBe("algorithm");
+    });
+
+    test("Verify method get algorithm is recibing the right value ", () => {
+        expect(analizing.algorithm).toBe("algorithm");
+    });
+
+    test("Verify method get imagePaths is recibing a wrong value ", () => {
+        expect(analizing.imagePaths).not.toBe("PathImage");
+    });
+
+    test("Verify method get searchWord is recibing the right value ", () => {
+        expect(analizing.searchWord).toBe("searchwords");
+    });
+
+    test("Verify method get percentage is recibing the right value ", () => {
+        expect(analizing.percentage).toBe("percentage");
+    });
+
+    test("Verify method get secondsToString is recibing the right value ", () => {
+        expect(analizing.secondsToString).toBe("secondsToString");
+    });
+
+    test("Verify method get pathImage is recibing the right value ", () => {
+        expect(analizing.pathImage).toBe("pathImage");
+    });
+});
