6b0ee12a341b40c4a7206e14b7dfafd59f587967
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-09 14:07:50 -0400

    Refactoring database endpoints and translate to other class

--- CONVERTER_SERVICE/src/controller/videoController.js
+++ CONVERTER_SERVICE/src/controller/videoController.js
@@ -37,33 +37,6 @@ const changeVideoFormat = async (req, res) => {
     //zipping.zipDownload(req, res);
 };
 
-const getData = async (req, res) => {
-    const data = await FileModel.find().lean().exec();
-    res.status(200).send({ data });
-};
-
-const findDataById = async (req, res) => {
-    const data = await FileModel.findById(req.params.id);
-    res.status(200).send({ data });
-};
-
-const deleteDataById = async (req, res) => {
-    const deleteData = await FileModel.deleteOne({ _id: req.params.id });
-    res.status(200).send({ deleteData });
-};
-
-const updateDataById = async (req, res) => {
-    const updatedData = await FileModel.updateOne(
-        { _id: req.params.id },
-        { $set: { name: req.body.name } }
-    );
-    res.status(200).send({ updatedData });
-};
-
 module.exports = {
-    changeVideoFormat,
-    getData,
-    deleteDataById,
-    findDataById,
-    updateDataById,
+    changeVideoFormat
 };
