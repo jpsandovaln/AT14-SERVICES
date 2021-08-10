c0f7d2dbf7ea43c1e33330e8b6b7dc95599f2848
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-10 18:27:57 -0400

    Add condition 'checksum verification'

--- CONVERTER_SERVICE/src/middleware/uploadFiles.js
+++ CONVERTER_SERVICE/src/middleware/uploadFiles.js
@@ -2,6 +2,7 @@ const path = require("path");
 const FileModel = require("../database/fileModel");
 const formidable = require('formidable');
 const FileUtil = require('../utilities/fileUtil');
+const Md5File = require("../utilities/checksum");
 
 require("dotenv").config("../../.env");
 const uploadPath = process.env.UPLOAD_PATH;
@@ -16,26 +17,33 @@ const updateFile = async (req, res, next) => {
     });
 
     req.fields = result;
-    const objectFile= await FileModel.findOne({checksum: result.checksum});
-    if(objectFile) {
-        req.fields.uploadpath = objectFile.path;
-        req.fields.filename = objectFile.name;
-        next();
+    const hash = Md5File.getMD5File(result.files.file.path);
+    const resulthash = hash;
+    if(result.checksum===resulthash){
+        const objectFile= await FileModel.findOne({checksum: result.checksum});
+        if(objectFile) {
+            req.fields.uploadpath = objectFile.path;
+            req.fields.filename = objectFile.name;
+            next();
+        } else {
+            req.fields.uploadpath = uploadPath + result.files.file.name;
+            req.fields.filename = result.files.file.name;
+            await FileUtil.copyFile(result.files.file.path, uploadPath, result.files.file.name);
+            const fileModel = new FileModel({
+                name: result.files.file.name,
+                path: uploadPath + result.files.file.name,
+                checksum: result.checksum,
+            });
+            fileModel.save(function (err, doc) {
+                if (err) return console.error(err);
+                console.log("Document inserted successfully!");
+            });
+            next();
+        }
     } else {
-        req.fields.uploadpath = uploadPath + result.files.file.name;
-        req.fields.filename = result.files.file.name;
-        console.log(uploadPath);
-        await FileUtil.copyFile(result.files.file.path, uploadPath, result.files.file.name);
-        const fileModel = new FileModel({
-            name: result.files.file.name,
-            path: uploadPath + result.files.file.name,
-            checksum: result.checksum,
-        });
-        fileModel.save(function (err, doc) {
-            if (err) return console.error(err);
-            console.log("Document inserted successfully!");
-        });
-        next();
+        res.send([
+            { message: "Invalid checksum, corrupt file" }
+        ]);
     }
 };
 
