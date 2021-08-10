d9fe1dd484c444d868f3fa6e0bd03222decd8daf
Esther Huarayo Ll <hll.esther@gmail.com>
2021-08-10 01:54:04 -0400

    Implementation promise function

--- CONVERTER_SERVICE/src/utilities/fileUtil.js
+++ CONVERTER_SERVICE/src/utilities/fileUtil.js
@@ -2,12 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 class FileUtil {
-    static copyFile(file, destFolder, newName) {
+    static async copyFile(file, destFolder, newName) {
         const source = fs.createReadStream(file);
         const dest = fs.createWriteStream(path.resolve(destFolder, newName));
-        source.pipe(dest);
-        source.on('end', () => { console.log('Succesfully copied'); });
-        source.on('error', (err) => { console.log(err); });
+        await new Promise ((resolve, rejects)=>{
+            source.pipe(dest);
+            source.on('end', () => {
+                console.log('Succesfully copied');
+                resolve();
+            });
+            source.on('error', (err) => { 
+                console.log(err);
+                rejects();
+            });
+        });
     }
 }
 
