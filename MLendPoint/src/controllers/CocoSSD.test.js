f82af120dba276615b4a976ae9c10f248487db41
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-08-11 00:06:58 -0400

    Change table position, and fix the percentaje result un machine learning intertface

--- MLendPoint/src/controllers/CocoSSD.test.js
+++
@@ -1,9 +0,0 @@
-const CocoSSD = require('./CocoSSD')
-const path = require("path");
-const { test, expect } = require("@jest/globals");
-
-test("Verify that model return an empty object array with parameters: Imagen, SearchWord, and Percentage those with values pathImagen, cow, and 0.1", async () => {
-    const obj = new CocoSSD(path.join(__dirname, '../../public/images/1.jpg'), 'cow', 0.1);
-    const result = await obj.getJSON();
-    expect(result).toEqual([]);
-}, 3000000);
