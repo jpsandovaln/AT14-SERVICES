1cb9d61f8922dfb586dc0c03029083e51275e4c0
Sergio <serqio.kay@gmail.com>
2021-08-17 00:00:57 -0400

    Adding research also add some progress in login with google and facebook

--- ReactUI/src/ui/modules/home/Home.jsx
+++ ReactUI/src/ui/modules/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { makeStyles } from "@material-ui/core";
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import GoogleLogin from "react-google-login";
+import FacebookLogin from "react-facebook-login";
 
 const styles = makeStyles((theme) => ({
 	root: {
@@ -18,31 +19,63 @@ const styles = makeStyles((theme) => ({
 	},
 }));
 
+const responseGoogle = (response) => {
+	console.log(response);
+};
+
+const responseFacebook = (response) => {
+	console.log(response);
+};
+
+const componentClicked = () => {
+	console.log("event onClick");
+};
+
 const Home = () => {
 	const classes = styles();
 	return (
 		<div className={classes.root}>
 			<h2>Home</h2>
 			<Grid
-			container
-			spacing={0}
-			direction="column"
-			alignItems="center"
-			justify="center"
-			style={{ minHeight: '100vh' }}
+				container
+				spacing={0}
+				direction="column"
+				alignItems="center"
+				justify="center"
+				style={{ minHeight: "100vh" }}
 			>
 				<Grid item xs={12}>
 					<Card>
-					<CardMedia
-						component="img"
-						alt="AT-14"
-						width="100%"
-						image="http://localhost:8081/at14.gif"
-						title="Contemplative Reptile"
-						/>		
+						<CardMedia
+							component="img"
+							alt="AT-14"
+							width="100%"
+							image="http://localhost:8081/at14.gif"
+							title="Contemplative Reptile"
+						/>
 					</Card>
-				</Grid>      
-			</Grid>			
+					<br></br>
+					<Card>
+						<Grid align="center">
+							<GoogleLogin
+								clientId="726629848709-g7mmqrelcc0fauka4vfimnbl16tvp2q5.apps.googleusercontent.com"
+								buttonText="Login"
+								onSuccess={responseGoogle}
+								onFailure={responseGoogle}
+								cookiePolicy={"single_host_origin"}
+							/>
+							<FacebookLogin
+								appId="235788085092305"
+								autoLoad={true}
+								fields="name,email,picture"
+								onClick={componentClicked}
+								callback={responseFacebook}
+							/>
+							,
+						</Grid>
+					</Card>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
