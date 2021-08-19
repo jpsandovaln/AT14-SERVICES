99d54c1099bf72868baccad0615b56b446bef981
Sergio <serqio.kay@gmail.com>
2021-08-19 01:10:21 -0400

    adding a mail text box and login text

--- ReactUI/src/ui/pages/login/LoginPage.jsx
+++ ReactUI/src/ui/pages/login/LoginPage.jsx
@@ -4,6 +4,8 @@ import {
 	Paper,
 	Avatar,
 	CssBaseline,
+	Typography,
+	TextField,
 } from "@material-ui/core";
 import React from "react";
 
@@ -22,6 +24,10 @@ const Login = () => {
 			width: theme.spacing(15),
 			height: theme.spacing(15),
 		},
+		form: {
+			width: "100%",
+			marginTop: theme.spacing(1),
+		},
 	}));
 	const classes = classStyles();
 	return (
@@ -33,6 +39,22 @@ const Login = () => {
 					alt="dog"
 					src="http://localhost:8081/at14.gif"
 				></Avatar>
+				<Typography component="h1" variant="h5">
+					Login
+				</Typography>
+				<form className={classes.form} noValidate>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+				</form>
 			</Paper>
 		</Container>
 	);
