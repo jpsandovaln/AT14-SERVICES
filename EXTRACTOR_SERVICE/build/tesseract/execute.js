fe0104067e07aedc5268033edcd201f8d5b1c1fd
markandeia <marcandeacabrera@gmail.com>
2021-08-20 11:47:46 -0400

    Create Model folder and ignore build results

--- EXTRACTOR_SERVICE/build/tesseract/execute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const imageToText_1 = require("./imageToText");
-const imageToText = new imageToText_1.ImageToText("eng", "https://telegram.org/file/811140100/2/maZcBXgwrmE.306486/5bd7c8f4708afe28f8");
-imageToText.initializeExtractor();
-imageToText.extractText();
-console.log(imageToText.getText());
