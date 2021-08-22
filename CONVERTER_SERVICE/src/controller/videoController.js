21179935ae57eda3985c7e12d80ba7731964c563
edwinPato <edwin.p.a.pato@gmail.com>
2021-08-22 12:32:58 -0400

    Fixing response for integration with UI

--- CONVERTER_SERVICE/src/controller/videoController.js
+++ CONVERTER_SERVICE/src/controller/videoController.js
@@ -8,41 +8,42 @@ const VideoServices = require("../middleware/videoService");
 require("dotenv").config("../../.env");
 
 const changeVideoFormat = async (req, res) => {
-    const nameFile = req.fields.filename;    
+    const nameFile = req.fields.filename;
     const resultName = Date.now();
-    const videoServices = new VideoServices(req.fields, nameFile, resultName);    
+    const videoServices = new VideoServices(req.fields, nameFile, resultName);
 
     const resultPathVideoFormat = await videoServices.changeVideoFormat();
-    const resultPathFrames = (req.fields.obtainFrames == 'true' ? await videoServices.obtainFrames() : "");
-    const resultPathAudio = (req.fields.obtainAudio == 'true' ? await videoServices.obtainAudio() : "");
+    const resultPathFrames = req.fields.obtainFrames == "true" ? await videoServices.obtainFrames() : "";
+    const resultPathAudio = req.fields.obtainAudio == "true" ? await videoServices.obtainAudio() : "";
     const zip = new Zip();
-    const pathFramesZip = (resultPathFrames != "" ? await zip.zipImages(resultPathFrames) : "");
-    const resultZipPath = await zip.zipFiles(outputPath + resultName, pathFramesZip, resultPathAudio, resultPathVideoFormat);
+    const pathFramesZip = resultPathFrames != "" ? await zip.zipImages(resultPathFrames) : "";
+    const resultZipPath = await zip.zipFiles(
+        outputPath + resultName,
+        pathFramesZip,
+        resultPathAudio,
+        resultPathVideoFormat
+    );
     const nameZipFile = path.basename(resultZipPath);
 
-    res.status(200).send({
-        name: resultName,
-        filePath: "http://localhost:8080/files/" + nameZipFile,
-        params: req.body
-    });
+    res.status(200).send([
+        {
+            name: nameFile,
+            filePath: "http://localhost:8080/files/" + nameZipFile,
+        },
+    ]);
 };
 
 const download = (req, res) => {
-    const fileName = req.params.name;
-    const directoryPath = outputPath + fileName; 
-
-    res.download(directoryPath, fileName, (err) => {
+    const filename = req.params.name;
+    const directoryPath = outputPath + filename;
+    res.download(directoryPath, filename, function (err) {
         if (err) {
-            res.status(500).send({
-                message: "Could not download the file. " + err,
-            });
-        }
-        else {
-            res.send({ message: "successful download" });
+            console.error(err);
         }
     });
 };
 
 module.exports = {
-    changeVideoFormat, download, 
+    changeVideoFormat,
+    download,
 };
