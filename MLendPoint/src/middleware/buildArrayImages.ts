9ae187f313f8b7581e2ad595766b4e563a151190
Sergio <serqio.kay@gmail.com>
2021-08-24 20:53:45 -0400

    changed file buildArrayImages.js to ts

--- MLendPoint/src/middleware/buildArrayImages.ts
+++ MLendPoint/src/middleware/buildArrayImages.ts
@@ -1,12 +1,16 @@
 class BuildArrayImages {
-    buildArrayImages(imagePaths, unzipOutput, filename) {
-        let files = [];
+    buildArrayImages(
+        imagePaths: string,
+        unzipOutput: string,
+        filename: string
+    ) {
+        let files: Array<object> = [];
         for (let index = 0; index < imagePaths.length; index++) {
             let element = {
                 originalname: imagePaths[index],
                 mimetype: "image/jpeg",
                 filename: imagePaths[index],
-                path: unzipOutput + "/" + filename + "/" + imagePaths[index],
+                path: unzipOutput + "/" + filename + "/" + imagePaths[index]
             };
             files.push(element);
         }
@@ -14,4 +18,4 @@ class BuildArrayImages {
     }
 }
 
-module.exports = BuildArrayImages;
+export default BuildArrayImages;
