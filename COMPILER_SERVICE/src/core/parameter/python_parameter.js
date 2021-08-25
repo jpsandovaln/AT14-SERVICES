68956423c1c05251035043b57c106e20ad28b1aa
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2021-08-24 23:05:02 -0400

    Added strategy pattern

--- COMPILER_SERVICE/src/core/parameter/python_parameter.js
+++ COMPILER_SERVICE/src/core/parameter/python_parameter.js
@@ -1,5 +1,11 @@
+const fs = require("fs");
+const path = require("path");
 const ParameterInvalidException = require("../../common/exception/parameter_exception");
 const Parameter = require("./parameter");
+const NotNullOrEmptyValidation = require("../../common/validation/not_null_or_empty_validation");
+const FileValidation = require("../../common/validation/file_validation");
+const FolderValidation = require("../../common/validation/folder_validation");
+const Context = require("../../common/validation/context");
 
 class PythonParameter extends Parameter {
 
@@ -17,9 +23,13 @@ class PythonParameter extends Parameter {
     }
 
     validate() {
-        if (!this._filePath || !this._pythonBinaryPath) {
-            throw new ParameterInvalidException('parameter invalid.', 'SAB-0124588');
-        }
+        const strategies = [];
+        strategies.push(new NotNullOrEmptyValidation("filePath", this._filePath));
+        strategies.push(new FileValidation(this._filePath, [".py"]));
+        strategies.push(new FolderValidation(this._pythonBinaryPath));
+
+        const context = new Context(strategies);
+        context.validate();
     }
 }
 
