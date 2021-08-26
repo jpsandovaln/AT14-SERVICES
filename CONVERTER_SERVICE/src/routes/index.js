fef7a51f7b58070f68ef4f1a754f101e7af0cb51
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-08-26 11:01:03 -0400

    Fix problem with a variable

--- CONVERTER_SERVICE/src/routes/index.js
+++ CONVERTER_SERVICE/src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { changeVideoFormat, download } = require("../controller/VideoController");
+const { changeVideoFormat, download } = require("../controller/videoController");
 const { framesZipML, downloadMLZip } = require("../controller/MLController");
 const {
     getData,
