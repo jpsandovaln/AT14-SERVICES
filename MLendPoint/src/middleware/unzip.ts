aa6d331390f495eb576518b0c159e164d3ac044d
Sergio <serqio.kay@gmail.com>
2021-08-27 10:47:27 -0400

    add some any in Unzip file

--- MLendPoint/src/middleware/unzip.ts
+++ MLendPoint/src/middleware/unzip.ts
@@ -2,7 +2,7 @@ import AdmZip from "adm-zip";
 import path from "path";
 
 class UnZip {
-    extractZip(zipPath, outPath) {
+    extractZip(zipPath: any, outPath: any): void {
         const nameFile = path.parse(zipPath).name;
         const zip = new AdmZip(zipPath);
         const zipEntries = zip.getEntries();
