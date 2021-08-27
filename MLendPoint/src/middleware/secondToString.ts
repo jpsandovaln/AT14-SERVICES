fc13b9195ed51ce780ca75ed05fab4d0e819ca56
Sergio <serqio.kay@gmail.com>
2021-08-27 11:03:59 -0400

    complete fix of seconds to string

--- MLendPoint/src/middleware/secondToString.ts
+++ MLendPoint/src/middleware/secondToString.ts
@@ -1,12 +1,15 @@
 class SecondsToString {
-    parse(seconds) {
-        let hour = Math.floor(seconds / 3600);
-        hour = hour < 10 ? "0" + hour : hour;
-        let minute = Math.floor((seconds / 60) % 60);
-        minute = minute < 10 ? "0" + minute : minute;
-        let second = seconds % 60;
-        second = second < 10 ? "0" + second : second;
-        return hour + ":" + minute + ":" + second;
+    parse(seconds: number): string {
+        const hour: number = Math.floor(seconds / 3600);
+        const hourToString: string =
+            hour < 10 ? "0" + hour.toString() : hour.toString();
+        const minute = Math.floor((seconds / 60) % 60);
+        const minuteToString: string =
+            minute < 10 ? "0" + minute.toString() : minute.toString();
+        const second = seconds % 60;
+        const secondsToString: string =
+            second < 10 ? "0" + second.toString() : second.toString();
+        return hourToString + ":" + minuteToString + ":" + secondsToString;
     }
 }
 
