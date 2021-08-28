f83be2ff518cd050df7b79d9d7c1aecf74361947
Sergio <serqio.kay@gmail.com>
2021-08-27 20:06:54 -0400

    modify functions of secondsToString

--- MLendPoint/src/middleware/secondToString.ts
+++ MLendPoint/src/middleware/secondToString.ts
@@ -1,12 +1,12 @@
 class SecondsToString {
-    parse(seconds: number): string {
-        const hour: number = Math.floor(seconds / 3600);
+    parse(seconds: string): string {
+        const hour: number = Math.floor(parseInt(seconds) / 3600);
         const hourToString: string =
             hour < 10 ? "0" + hour.toString() : hour.toString();
-        const minute = Math.floor((seconds / 60) % 60);
+        const minute = Math.floor((parseInt(seconds) / 60) % 60);
         const minuteToString: string =
             minute < 10 ? "0" + minute.toString() : minute.toString();
-        const second = seconds % 60;
+        const second = parseInt(seconds) % 60;
         const secondsToString: string =
             second < 10 ? "0" + second.toString() : second.toString();
         return hourToString + ":" + minuteToString + ":" + secondsToString;
