4f2f2bb2ddda5ef2533209661cc75fcdf0f6313f
Sergio <serqio.kay@gmail.com>
2021-08-27 23:02:58 -0400

    fix unzip.ts errors

--- MLendPoint/src/middleware/unzip.ts
+++ MLendPoint/src/middleware/unzip.ts
@@ -1,12 +1,8 @@
 import AdmZip from "adm-zip";
-import path from "path";
 
 class UnZip {
-    extractZip(zipPath: any, outPath: any): void {
-        const nameFile = path.parse(zipPath).name;
+    extractZip(zipPath: string, outPath: string): void {
         const zip = new AdmZip(zipPath);
-        const zipEntries = zip.getEntries();
-
         zip.extractAllTo(outPath, true);
     }
 }
