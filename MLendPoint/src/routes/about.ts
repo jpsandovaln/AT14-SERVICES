f57a5968b7b76a7ef471ca376fbdc23febe3e6a3
Sergio <serqio.kay@gmail.com>
2021-08-28 23:41:47 -0400

    refactor about.ts to a class

--- MLendPoint/src/routes/about.ts
+++ MLendPoint/src/routes/about.ts
@@ -1,8 +1,20 @@
-import express from "express";
-const router = express.Router();
+import { Request, Response, Router } from "express";
 
-router.get("/", (req, res) => {
-    res.send("I'm about");
-});
+class AboutPage {
+    router: Router;
 
-export default router;
+    constructor() {
+        this.router = Router();
+        this.routes();
+    }
+    routes() {
+        this.router.get("/", (req: Request, res: Response) => {
+            res.send("I'm about");
+        });
+    }
+}
+
+const aboutRouter = new AboutPage();
+aboutRouter.routes();
+
+export default aboutRouter.router;
