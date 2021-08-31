6d78498560551b45a2421b04d0db20e4fe439224
markandeia <marcandeacabrera@gmail.com>
2021-08-31 11:45:54 -0400

    Create mock with another type of name

--- EXTRACTOR_SERVICE/__test__/extractCroppedImage.test.ts
+++ EXTRACTOR_SERVICE/__test__/extractCroppedImage.test.ts
@@ -2,10 +2,11 @@ jest.mock("../src/model/tesseract/extractCroppedImage.ts");
 
 import { createWorker } from "tesseract.js";
 import { ExtractCroppedImage } from "../src/model/tesseract/extractCroppedImage";
+import { Extractor } from "../src/model/tesseract/extractor";
 import { ICropped } from "../src/model/tesseract/interfaces/iCropped";
 
 describe("ExtractCroppedImage", () => {
-	it("works", () => {
+	it("works", async () => {
 		expect.assertions(1);
 		const worker = createWorker();
 		const rectanglePart = {
@@ -20,9 +21,10 @@ describe("ExtractCroppedImage", () => {
 			path: "https://telegram.org/file/811140100/2/maZcBXgwrmE.306486/5bd7c8f4708afe28f8",
 			rectangle: rectanglePart,
 		};
-		return new ExtractCroppedImage(imageToCropped)
-			.extract()
-			.then((result) => expect(result).toEqual("Text"));
+		const extractorCroppedImage: Extractor = new ExtractCroppedImage(imageToCropped);
+		expect(extractorCroppedImage.extract().resolves.toEqual('Text'));
+		// return extractorCroppedImage.extract()
+		// 	.then((result:any) => expect(result).toEqual("Text"));
 	});
 
 	// it("not works", () => {
