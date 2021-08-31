fd9bd3b3543026aa0dccd5fc6fefb1a72c8d8fd6
markandeia <marcandeacabrera@gmail.com>
2021-08-31 12:33:14 -0400

    Remove execute file

--- EXTRACTOR_SERVICE/src/model/tesseract/extractToPDF.ts
+++ EXTRACTOR_SERVICE/src/model/tesseract/extractToPDF.ts
@@ -12,7 +12,7 @@ export class ExtractToPDF extends Extractor {
 			await this.loadWorker();
 			await this.worker.recognize(this.path);
 			const { data } = await this.worker.getPDF("Tesseract OCR Result");
-			fs.writeFileSync("tesseract-ocr-result.pdf", Buffer.from(data));
+			fs.writeFileSync("public/uploads/tesseract-ocr-result.pdf", Buffer.from(data));
 			return data;
 		} catch (error) {
 			throw new PDFToImageException(
