c085e6f677cf9573ec42b7cb62da3aa4c33c580d
Sergio <serqio.kay@gmail.com>
2021-09-05 10:04:52 -0400

    add functinality of create folder in converter service

--- CONVERTER_SERVICE/server.js
+++ CONVERTER_SERVICE/server.js
@@ -2,20 +2,33 @@ const cors = require("cors");
 const express = require("express");
 const Routes = require("./src/routes");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
+const createFolders = require("./createFolder");
 
-conexionString = 'mongodb://' + process.env.IP_MONGO + ':27018/converterDB';
+createFolders();
 
-mongoose.connect(conexionString,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(db => console.log('Db is connected to', db.connection.host))
-    .catch(err=> console.error(err));
+conexionString = "mongodb://" + process.env.IP_MONGO + ":27018/converterDB";
+
+mongoose
+    .connect(conexionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then((db) => console.log("Db is connected to", db.connection.host))
+    .catch((err) => console.error(err));
 
 global.__basedir = __dirname;
 
 class Index {
     constructor(
-        corsOptions = { origin: ["http://localhost:8080","http://localhost:4000","http://localhost:8081"] },
+        corsOptions = {
+            origin: [
+                "http://localhost:8080",
+                "http://localhost:4000",
+                "http://localhost:8081"
+            ]
+        },
         port = process.env.PORT_CONVERTER
     ) {
         app.use(cors(corsOptions));
