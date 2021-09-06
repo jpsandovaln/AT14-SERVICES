66425c16ab86d5709eb695af29a415aec895ff57
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-09-06 02:13:03 -0400

    Add changes on UI to consume apollo with video converter

--- ReactUI/src/ui/pages/Converter/ConverterPage.jsx
+++ ReactUI/src/ui/pages/Converter/ConverterPage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import SideBar from "../../modules/sidebar/SideBar";
 import Converter from "../../modules/converter/Converter";
+import ApolloClient from "apollo-client";
+import { createUploadLink } from "apollo-upload-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
+const httpLink = createUploadLink({
+	uri: "http://localhost:4000/graphql",
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
 const ConverterPage = ({ props }) => {
-	return <SideBar page={Converter} />;
+	return (
+		<ApolloProvider client={client}>
+			<SideBar page={Converter} />
+		</ApolloProvider>
+	);	
 };
 
 export default ConverterPage;
