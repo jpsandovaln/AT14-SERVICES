14791d596f839ec90acec157678326d6a3487d30
Carlos Tito Caceres <viri_nxg@hotmail.com>
2021-09-06 00:26:19 -0400

    Add new dependences and fix troubles while sending files

--- APOLLO_SERVER2/src/resolvers.js
+++ APOLLO_SERVER2/src/resolvers.js
@@ -3,14 +3,15 @@ import FormData from "form-data";
 import axios from "axios";
 import * as fs from "fs";
 import {GraphQLUpload} from "graphql-upload";
-import http from "http"; // or 'https' for https:// URLs
+import wget from "node-wget-promise";
+require("dotenv").config();
+const uploadPath = process.env.UPLOAD_PATH;
 
-const FileData = [];
-const FileData1 = [];
+let FileData = [];
+let FileData1 = [];
 
 const storeUpload = async ({stream, filename, mimetype}) => {
     const id = shortid.generate();
-    const pathOLD = `images/${id}-${filename}`;
     const path = `images/${filename}`;
 
     return new Promise((resolve, reject) =>
@@ -67,57 +68,37 @@ const resolvers = {
             fs.mkdir("images", {recursive: true}, (err) => {
                 if (err) throw err;
             });
-
+            FileData1 = [];
             const uploadFile = await processUpload(args.file);
-            console.log("aaaaaaaaaaaaaaaaaaarg: "+args.file);
             const dataArray1 = new FormData();
             dataArray1.append("file", fs.createReadStream(uploadFile.path));
             dataArray1.append("obtainFrames", "true");
             dataArray1.append("frameScale", "400");
             dataArray1.append("grayScale", "true");
-            const urlML1 = "http://localhost:8080/frames";
-            console.log(urlML1);
-            console.log(dataArray1.getHeaders());
-            const res =
-             await axios.post(
-                urlML1,
-                dataArray1,
-                /*{
-                    maxContentLength: Infinity,
-                    maxBodyLength: Infinity,
-                },*/
-                {
-                    headers: dataArray1.getHeaders(),
-                }
-            );
-            console.warn(
-                res.data
-            );
-            console.log(res.data[0].filePath);
-
-            const file = fs.createWriteStream('images/123.zip');
-            const request = http.get(res.data[0].filePath, function (response) {
-                response.pipe(file);
+            const urlML1 = "http://localhost:4050/frames";
+            const res = await axios.post(urlML1, dataArray1, {
+                headers: dataArray1.getHeaders(),
             });
-            var newFile = fs.createReadStream(file.path);
+
+            await wget(res.data[0].filePath,{
+                output: "images/"+res.data[0].name+".zip"
+              });
 
             const dataArray = new FormData();
             dataArray.append("searchWord", args.searchWord);
             dataArray.append("algorithm", args.algorithm);
             dataArray.append("percentage", args.percentage);
-            dataArray.append("zipFile", fs.readFileSync('images/123.zip'), '123.zip');
+            dataArray.append("zipFile", fs.readFileSync("images/"+res.data[0].name+".zip"), res.data[0].name+".zip");
 
             const urlML = "http://localhost:8085/analizeZip";
-            console.log(urlML);
             const res1 = await axios.post(urlML, dataArray, {
                 headers: dataArray.getHeaders(),
             });
 
             Array.prototype.push.apply(FileData1, res1.data);
-            console.log(FileData1)
+            console.log(FileData1);
 
             return FileData1;
-
         },
     },
 };
