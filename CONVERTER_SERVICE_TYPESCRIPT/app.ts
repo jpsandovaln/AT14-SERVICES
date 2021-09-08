8954967a3e56d38c482f4546450949ad68efacac
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-09-08 12:03:56 -0400

    Adding the app.ts

--- CONVERTER_SERVICE_TYPESCRIPT/app.ts
+++ CONVERTER_SERVICE_TYPESCRIPT/app.ts
@@ -1,1 +1,42 @@
-console.log("Tu puedes hacer esto....test");
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import morganMiddleware from "./src/middleware/common/morgan";
+import logger from "./src/utilities/logger";
+
+const app = express();
+class Index {
+    port: string | undefined;
+    corsOptions: object;
+
+    constructor() {
+        dotenv.config();
+
+        this.port = process.env.PORT_CONVERTER;
+        this.corsOptions = {
+            origin: [
+                "http://localhost:8080",
+                "http://localhost:4000",
+                "http://localhost:8081",
+            ],
+        };
+        this.initConfigs();
+        //new Routes(app);
+        this.init();
+    }
+
+    private initConfigs() {
+        app.use(cors(this.corsOptions));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use(morganMiddleware);
+    }
+
+    private init() {
+        app.listen(this.port, () => {
+            logger.debug("Running at http://localhost:" + this.port);
+        });
+    }
+}
+
+new Index();
