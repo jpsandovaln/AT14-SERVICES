67490cc5ef6685244e0aa1ad9748fba32bf4d085
Cristian Torrico Vasquez <cristian.torrico@fundacion-jala.org>
2021-09-10 00:04:09 -0400

    Adding the new properties to the file

--- CONVERTER_SERVICE_TYPESCRIPT/src/utilities/property.ts
+++ CONVERTER_SERVICE_TYPESCRIPT/src/utilities/property.ts
@@ -1,36 +1,35 @@
-export class Property{
+export class Property {
     private static isValid(process_env: any): string {
-        if(process_env != undefined)
-            return process_env;
+        if (process_env != undefined) return process_env;
         else {
             console.log("El punto env: " + process_env);
             return "";
-        }            
+        }
     }
 
     static getFFmpegPath(): string {
         return this.isValid(process.env.FFMPEG_PATH);
-    } 
+    }
 
     static getExifToolPath(): string {
         return this.isValid(process.env.EXIFTOOL_PATH);
-    } 
+    }
 
     static getMagickPath(): string {
         return this.isValid(process.env.MAGICK_PATH);
-    } 
-    
+    }
+
     static getUploadPath(): string {
         return this.isValid(process.env.UPLOAD_FILE);
-    } 
+    }
 
     static getOutputPath(): string {
         return this.isValid(process.env.OUTPUT_PATH);
-    } 
+    }
 
     static getImagePath(): string {
         return this.isValid(process.env.IMAGE_PATH);
-    } 
+    }
 
     static getVideoPath(): string {
         return this.isValid(process.env.VIDEO_PATH);
@@ -42,5 +41,11 @@ export class Property{
 
     static getZipPath(): string {
         return this.isValid(process.env.ZIP_PATH);
+    }
+    static getPort(): string {
+        return this.isValid(process.env.PORT_CONVERTER);
+    }
+    static getBaseUrl(): string {
+        return this.isValid(process.env.BASE_URL_CONVERTER);
     }
 }
