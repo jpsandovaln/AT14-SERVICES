1c0b16f7ce49d56f609b985a1cd59a6bad3d21eb
Milena-Rodriguez-Montecinos <mile.raychel@gmail.com>
2021-09-10 01:01:04 -0400

    Refactoring audioServices with Property class

--- CONVERTER_SERVICE_TYPESCRIPT/src/middleware/audio/audioServices.ts
+++ CONVERTER_SERVICE_TYPESCRIPT/src/middleware/audio/audioServices.ts
@@ -1,35 +1,25 @@
 import { Compiler } from "../../model/common/compiler/compiler";
 import { BuildCmdAudio } from "../../model/audio/buildCmd/buildCmdAudio";
 import { Parameters } from "../../model/common/parameter/parameters";
+import { Property } from "../../utilities/property"
 var fs = require('fs');
 export class AudioServices{
     public params: Parameters;
     private compiler: Compiler;
-    private nameFile: string;
     private resultName: string;
     private filePath: string;
-    private codecPath: string;
-    private uploadPath: string;
-    private outputPath: string;
-    private audioPath: string;
     
-    constructor(json: object, nameFile: string, resultName: string, codecPath: string, uploadPath: string, outputPath: string, audioPath: string) {
+    constructor(json: object, nameFile: string, resultName: string, codecPath: string) {
         this.params = new Parameters(json); 
         this.compiler = new Compiler();
-        this.nameFile = nameFile;
         this.resultName = resultName;
-        this.filePath = uploadPath + nameFile;
-        this.codecPath = codecPath;
-        this.uploadPath = uploadPath;
-        this.outputPath = outputPath;
-        this.audioPath = audioPath;
+        this.filePath = Property.getUploadPath() + nameFile;
     } 
 
     async ChangeAudioFormat() {
-        let cmdChangeAudioFormat = new BuildCmdAudio (this.params, this.codecPath, this.filePath, this.outputPath, this.resultName);
+        let cmdChangeAudioFormat = new BuildCmdAudio (this.params, Property.getFFmpegPath(), this.filePath, Property.getOutputPath(), this.resultName);
         await this.compiler.execute(cmdChangeAudioFormat.returnCmd());    
-        const resultPathAudio = this.audioPath + this.resultName + this.params.getParameter('audioFormat');
+        const resultPathAudio = Property.getAudioPath + this.resultName + this.params.getParameter('audioFormat');
         return resultPathAudio;
-
     } 
 }
