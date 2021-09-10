68e9af03a9dcab12afad730bde62eb59e484fe15
markandeia <marcandeacabrera@gmail.com>
2021-09-10 06:14:15 -0400

    Remove unnecessary commented lines

--- EXTRACTOR_SERVICE/tests/extractToText.test.ts
+++ EXTRACTOR_SERVICE/tests/extractToText.test.ts
@@ -1,16 +1,10 @@
-import Tesseract, {
-	createWorker,
-	recognize,
-	RecognizeResult,
-} from "tesseract.js";
+import { createWorker } from "tesseract.js";
 import { Code } from "../src/common/code";
 import { ExtractorException } from "../src/common/exception/extractorException";
-import { TextToImageException } from "../src/common/exception/textToImageException";
 import { StatusCode } from "../src/common/statusCode";
 import { Extractor } from "../src/model/tesseract/extractor";
 import { ExtractToText } from "../src/model/tesseract/extractToText";
 import { IBase } from "../src/model/tesseract/interfaces/iBase";
-import { instance, mock, when } from "ts-mockito";
 
 var properties: IBase = {
 	worker: createWorker(),
@@ -40,47 +34,3 @@ describe("Parameters Exception", () => {
 		expect(() => extractToText.validateParameter()).toThrow(exception);
 	});
 });
-
-/* This method doesn't work correctly in the line 49 I have the error.
-	* I try to use .thenResolve() of mockito library, but I get another error.
-*/
-describe("Tests for extract() exception.", () => {
-	it("Text to image Exception", () => {
-		let mockedWorker: any = mock(createWorker());
-		let data: RecognizeResult = mock({});
-		let mockedRecognizeResult: Promise<RecognizeResult> = mock(data);
-		when(createWorker().recognize("test")).thenReturn(
-			mockedRecognizeResult
-		);
-		properties.worker = mockedWorker;
-		properties.language = "eng";
-		const extractToText: Extractor = new ExtractToText(properties);
-		const exception: ExtractorException = new TextToImageException(
-			"error",
-			StatusCode.InternalServerError,
-			Code.EXTRACTOR_ERROR_06
-		);
-		expect(extractToText.extract()).toThrow(exception);
-	});
-});
-
-/* Also I was trying this, but doesn't work too */
-
-// describe("Test for extract function", () => {
-// 	it("Testing...", () => {
-// 		let mockedExtractor = mock(ExtractToText);
-// 		when(mockedExtractor.extract()).thenThrow(new TextToImageException(
-// 			"error",
-// 			StatusCode.InternalServerError,
-// 			Code.EXTRACTOR_ERROR_06
-// 		));
-// 		properties.language = "eng";
-// 		const extractToText: Extractor = instance(mockedExtractor);
-// 		const exception: ExtractorException = new TextToImageException(
-// 			"error",
-// 			StatusCode.InternalServerError,
-// 			Code.EXTRACTOR_ERROR_06
-// 		);
-// 		expect(extractToText.extract()).toThrow(exception);
-// 	});
-// });
