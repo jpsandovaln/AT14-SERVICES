8d96505865856f68b63734d905bb42274769fcfa
Milena-Rodriguez-Montecinos <mile.raychel@gmail.com>
2021-09-12 22:55:12 -0400

    Checking errors

--- CONVERTER_SERVICE_TYPESCRIPT/src/routes/audio/routesAudio.ts
+++ CONVERTER_SERVICE_TYPESCRIPT/src/routes/audio/routesAudio.ts
@@ -1,9 +1,7 @@
 import express, { Request, Response } from "express";
 import { AudioController } from "../../controller/audio/audioController";
-import { MetadataController } from "../../controller/metadata/metadataController";
 import { UploadFile } from "../../middleware/common/uploader/uploadFile.";
 import { DownloadFile } from "../../controller/download/downloadFile";
-import { UploadFactory } from "../../middleware/common/uploader/uploadFactory";
 import { Property } from "../../utilities/property";
 import { Routes } from "../routes";
 
@@ -14,10 +12,8 @@ export class RoutesAudio extends Routes {
     }
 
     public getRoutes():void {
-        router.post("/audioConverter", [UploadFactory.getInstance(Property.getDBValidation()).uploadFile], new AudioController().AudioProcess);
+        router.post("/audioConverter", [new UploadFile().uploadFile], new AudioController().AudioProcess);
         router.get("/files/:name", new DownloadFile(Property.getOutputPath()).download);
-        router.post("/filesMetadata", [new UploadFile().uploadFile], new MetadataController().MetadataProcess);
-        router.get("/filesMetadata/:name", new DownloadFile(Property.getOutputPath()).download);
         this.app.use(router);
     }
 }
