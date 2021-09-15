702541b05a67434b4e85ca5c13d6a761678ac17a
edwinPato <edwin.p.a.pato@gmail.com>
2021-09-15 11:35:15 -0400

    Clean converter service for use with JS only

--- CONVERTER_SERVICE/src-2/middleware/upload.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IUpload {
-    mainPath: string | undefined;
-    maxSize: number;
-}
--- CONVERTER_SERVICE/src-2/model/converter/video/cmd/cmdVF/vf.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from "../cmd";
-
-export abstract class Vf extends Command {    
-    constructor(parameters: any) { 
-        super(parameters);
-    }    
-}
