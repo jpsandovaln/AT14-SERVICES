7430e854621ec82f301b025c9fe322ca3c990129
Sergio <serqio.kay@gmail.com>
2021-09-15 07:07:31 -0400

    add home get test

--- MLendPoint/src/app.test.ts
+++ MLendPoint/src/app.test.ts
@@ -1,8 +1,14 @@
 import app from "./app";
 import request from "supertest";
 
-test("test about", async () => {
+test("test about get", async () => {
     const result = await request(app).get("/about");
     expect(result.text).toEqual("I'm about");
+    expect(result.statusCode).toEqual(200);
+});
+
+test("test home get ", async () => {
+    const result = await request(app).get("/");
+    expect(result.text).toEqual("I'm home");
     expect(result.statusCode).toEqual(200);
 });
